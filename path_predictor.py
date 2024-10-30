ecd3af6cdbfe0c8adec0179b3a938fb394120bd1
cccsolver <109029273+cccsolver@users.noreply.github.com>
2024-10-30 20:37:07 -0300

    Update path_predictor.py

--- path_predictor.py
+++ path_predictor.py
@@ -20,6 +20,8 @@ nltk.download('punkt')
 nltk.download('punkt_tab')
 nltk.download('stopwords')
 nltk.download('wordnet')
+nltk.download('averaged_perceptron_tagger')
+nltk.download('omw-1.4')
 
 stop_words_ = set(stopwords.words('english'))
 wn = WordNetLemmatizer()
